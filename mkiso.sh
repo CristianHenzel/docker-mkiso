a570c08106455aba27a592ebef01ce16a6a59db3
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2019-08-17 14:02:10 +0000

    Simplify mkiso.sh

--- mkiso.sh
+++ mkiso.sh
@@ -1,42 +1,18 @@
 #!/bin/bash
 
-# Reset
-Color_Off='\033[0m'	# Text Reset
+set -exo pipefail
 
-# Regular Colors
-Red='\033[0;31m'	# Red
-Green='\033[0;32m'	# Green
+SPACER="------------------------------"
+DEB_ISO_URL="http://ftp.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/gtk/mini.iso"
 
-error_die () {
-	echo -e "${Red}ERROR:${Color_Off} $1"
-	exit 1
-}
-
-file_download () {
-	local URL; URL=$1
-	local FILE; FILE=$2
-	echo -n "       "
-	wget --progress=dot -O $FILE $URL 2>&1 | grep --line-buffered "%" | \
-		sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b\b\b\b\b (%4s).", $2)}'
-	echo
-}
-
-
-popd () {
-	command popd "$@" > /dev/null
-}
-
-pushd () {
-	command pushd "$@" > /dev/null
-}
-
-show_info () {
-	echo -e ${2} "${Green}$(date -R) ---${Color_Off} $1."
-}
+if [ "${1}" != "entry" ]; then
+	/var/app/mkiso.sh "entry" 2>&1 | ts
+	exit 0
+fi
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Replace env variables
-while IFS='=' read -r name value ; do
+while IFS='=' read -r name value; do
 	if [[ "${name}" = "MKISO_"* ]]; then
 		echo "${name}=\"${value}\"" >> /tmp/mkiso.env
 		VARNAME=$(echo "${name}" | cut -d_ -f2-)
@@ -51,20 +27,20 @@ mkdir "${TMPDIR}/extracted" "${TMPDIR}/initrd"
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Download mini.iso
-show_info "Downloading mini.iso" -n
-file_download "http://ftp.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/gtk/mini.iso" "${TMPDIR}/mini.iso"
+echo "${SPACER} Downloading mini.iso"
+wget --progress=dot:giga "${DEB_ISO_URL}" -O "${TMPDIR}/mini.iso"
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Extract image
-show_info "Extracting image"
-xorriso -osirrox on -indev "${TMPDIR}/mini.iso" -extract / "${TMPDIR}/extracted" &>>"${TMPDIR}/xorriso.log" || error_die "Could not extract image"
+echo "${SPACER} Extracting image"
+xorriso -osirrox on -indev "${TMPDIR}/mini.iso" -extract / "${TMPDIR}/extracted" &>>"${TMPDIR}/xorriso.log"
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Customizing image
-show_info "Customizing image"
+echo "${SPACER} Customizing image"
 pushd "${TMPDIR}/extracted"
 mv "/tmp/mkiso.env" .
-rm -rf *.{bin,c32,cfg,exe,ini,txt} .disk g2ldr* || error_die "Could not remove superfluous files"
+rm -rf -- *.{bin,c32,cfg,exe,ini,txt} .disk g2ldr*
 perl -i -pe "BEGIN{undef $/;} s/submenu.*{.*}//smg" "boot/grub/grub.cfg"
 sed -i "s/\/isolinux//" "boot/grub/grub.cfg"
 sed -i "N; s/440 1\nmenuentry/440 1\nset timeout=5\n\nmenuentry/" "boot/grub/grub.cfg"
@@ -73,35 +49,34 @@ popd
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Customizing EFI
-show_info "Customizing EFI"
-mcopy -i "${TMPDIR}/extracted/boot/grub/efi.img" ::efi/boot/bootx64.efi "${TMPDIR}/bootx64.efi" || error_die "Could not extract efi.img"
-mdel -i "${TMPDIR}/extracted/boot/grub/efi.img" ::efi/boot/bootx64.efi || error_die "Could not remove old efi.img"
-GRUBCFG_OFFSET=$(grep -aob "/.disk/info" "/${TMPDIR}/bootx64.efi" | cut -d: -f1)
-echo "/mkiso.env " | dd of="/${TMPDIR}/bootx64.efi" bs=1 seek=${GRUBCFG_OFFSET} count=11 conv=notrunc 2>/dev/null || error_die "Could not update efi grub.cfg"
-mcopy -i "${TMPDIR}/extracted/boot/grub/efi.img" "${TMPDIR}/bootx64.efi" ::efi/boot/bootx64.efi || error_die "Could not replace efi.img"
+echo "${SPACER} Customizing EFI"
+mcopy -i "${TMPDIR}/extracted/boot/grub/efi.img" ::efi/debian/grub.cfg "${TMPDIR}/bootx64.efi"
+mdel -i "${TMPDIR}/extracted/boot/grub/efi.img" ::efi/debian/grub.cfg
+sed -i "s/mkiso.env/.disk\/info/" "${TMPDIR}/bootx64.efi"
+mcopy -i "${TMPDIR}/extracted/boot/grub/efi.img" "${TMPDIR}/bootx64.efi" ::efi/debian/grub.cfg
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Customize initrd
-show_info "Unpacking initrd"
+echo "${SPACER} Unpacking initrd"
 pushd "${TMPDIR}/initrd"
-zcat "${TMPDIR}/extracted/initrd.gz" | cpio --quiet -i -d || error_die "Could not extract initrd"
+zcat "${TMPDIR}/extracted/initrd.gz" | cpio --quiet -i -d
 
 # Add custom data
-cp "/var/app/preseed.cfg" "${TMPDIR}/initrd/preseed.cfg" || error_die "Could not copy preseed"
+cp "/var/app/preseed.cfg" "${TMPDIR}/initrd/preseed.cfg"
 
-mkdir -p "${TMPDIR}/initrd/usr/share/debootstrap/scripts" || error_die "Could not create debootstrap scripts directory"
-ln -sf "sid" "${TMPDIR}/initrd/usr/share/debootstrap/scripts/testing" || error_die "Could not create testing script link"
+mkdir -p "${TMPDIR}/initrd/usr/share/debootstrap/scripts"
+ln -sf "sid" "${TMPDIR}/initrd/usr/share/debootstrap/scripts/testing"
 
 # Repack initrd
-show_info "Repacking initrd"
-find . | cpio --quiet -o -H newc | gzip -9 > "${TMPDIR}/extracted/initrd.gz" || error_die "Could not repack initrd"
+echo "${SPACER} Repacking initrd"
+find . | cpio --quiet -o -H newc | gzip > "${TMPDIR}/extracted/initrd.gz"
 popd
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Repack image
-show_info "Repacking image"
+echo "${SPACER} Repacking image"
 xorriso -as mkisofs -o "/data/debian-installer-$(date +%F-%H_%M).iso" -c "boot.cat" -J -joliet-long \
 	-eltorito-alt-boot -e "boot/grub/efi.img" -no-emul-boot \
-	"${TMPDIR}/extracted" -- &>>"${TMPDIR}/xorriso.log" || error_die "Could not repack image"
+	"${TMPDIR}/extracted" -- &>>"${TMPDIR}/xorriso.log"
 
-show_info "Done"
+echo "${SPACER} Done"
